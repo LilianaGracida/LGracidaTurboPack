703cbad76fa06d42487f869bb20bf3dd69039f89
LilianaGracida <lgbrioso@gmail.com>
2023-05-25 18:27:11 -0600

    Add project files.

---
+++ ML/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Usuario
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public string Sexo { get; set; }
+        public string Foto { get; set; }
+        public List<Object> Usuarios { get; set; }
+    }
+}
